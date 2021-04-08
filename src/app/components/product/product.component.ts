d1a679c78cef8a312c2967ece42d3b97843c5fce
Fikret Gözütok <fikret_gztk@hotmail.com>
2021-04-08 06:41:23 +0300

    Week 19

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CartItems } from 'src/app/models/cartItems';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -11,8 +14,13 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
+  filterText:string;
 
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) {}
+  constructor(
+    private productService:ProductService,
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService,
+    private cartService:CartService) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -37,5 +45,10 @@ export class ProductComponent implements OnInit {
       this.products = response.data;
       this.dataLoaded = true;
     });
+  }
+
+  addToCart(product:Product){
+    this.toastrService.success("Sepete eklendi", product.productName);
+    this.cartService.addToCart(product);
   }
 }
